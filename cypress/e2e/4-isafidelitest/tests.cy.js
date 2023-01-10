2635d140d186f47973a44a25e4c06b598775cfa1
David ROCHELET <drochelet@groupeisgri.com>
2023-01-10 16:53:47 +0100

    Refactoring

--- cypress/e2e/4-isafidelitest/tests.cy.js
+++ cypress/e2e/4-isafidelitest/tests.cy.js
@@ -12,25 +12,37 @@ context('Application IsaFideliTest', () => {
     describe('Login to the app', () => {
 
         it.only('As a user \n I can create an account', () => {
-            cy
-                .get('.mat-card-actions > .mat-focus-indicator')
-                .click()
-            cy.get('[formcontrolname="nom"]').type(USER[0].name)
-            cy.get('[formcontrolname="prenom"]').type(USER[0].username)
-            cy.get('[formcontrolname="adresse"]').type(USER[0].address.street)
-            cy.get('[formcontrolname="codePostal"]').type(USER[0].address.zipcode)
-            cy.get('[formcontrolname="ville"]').type(USER[0].address.city)
-            cy.get('[formcontrolname="email"]').type(USER[0].email)
-            cy.get('[formcontrolname="dateNaissance"]').type('01012022')
-            cy.get('[formcontrolname="numeroCarteFidelite"]').type(USER[0].username)
-            cy.get('#mat-slide-toggle-1-input').click({ force: true })
-            cy.contains('Enregistrer').click()
-            cy
-                .writeFile(
-                    'userdata.json',
-                    window.localStorage
-                        .getItem('isafidelitest_compte')
-                )
+            cy.accederAuFormulaireInscription()
+            rempliFormulaireInscription()
+            sauverDonneesSession()
+
+            function sauverDonneesSession() {
+                /* cy
+                    .writeFile(
+                        'userdata.json',
+                        window.localStorage
+                            .getItem('isafidelitest_compte')
+                    ) */
+            }
+
+            function accederAuFormulaireInscription() {
+                cy
+                    .get('.mat-card-actions > .mat-focus-indicator')
+                    .click()
+            }
+
+            function rempliFormulaireInscription() {
+                cy.get('[formcontrolname="nom"]').type(USER[0].name)
+                cy.get('[formcontrolname="prenom"]').type(USER[0].username)
+                cy.get('[formcontrolname="adresse"]').type(USER[0].address.street)
+                cy.get('[formcontrolname="codePostal"]').type(USER[0].address.zipcode)
+                cy.get('[formcontrolname="ville"]').type(USER[0].address.city)
+                cy.get('[formcontrolname="email"]').type(USER[0].email)
+                cy.get('[formcontrolname="dateNaissance"]').type('01012022')
+                cy.get('[formcontrolname="numeroCarteFidelite"]').type(USER[0].username)
+                cy.get('#mat-slide-toggle-1-input').click({ force: true })
+                cy.contains('Enregistrer').click()
+            }
         })
 
         it('As a user \n I can connect to the app', () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -22,4 +22,7 @@
 //
 //
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('accederAuFormulaireInscription', () => { 
+    cy.get('.mat-card-actions > .mat-focus-indicator')
+    .click()
+ })
